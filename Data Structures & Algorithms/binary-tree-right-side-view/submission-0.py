bc8253af22ce70ae27a5d5786469d4053b6d4669
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-04-08 18:57:32 +0100

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # for every layer, return rightmost
+        # dicard left? no. go left,
+        # return val if mroe depth
+
+        # first idea: use BFS, return last of every sub list. 
+        ans = []
+        def bfs(root):
+            if root is None: return []
+            q = deque([root])
+
+            while q:
+                level = []
+                for i in range(len(q)):
+                    node = q.popleft()
+                    level.append(node.val)
+                    if node.right:
+                        q.append(node.right)
+                    if node.left:
+                        q.append(node.left)
+
+                ans.append(level)
+
+        bfs(root)
+        final = []
+        print(ans)
+        # now i need last of every sublist"
+        for i in ans:
+            final.append(i[0])
+
+        return final
+                        
+            
+        
