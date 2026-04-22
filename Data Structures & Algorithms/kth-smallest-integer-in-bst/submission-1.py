a0ceabec4cbd0b338d2782bf76583ef8aea413ed
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-04-22 20:01:46 +0100

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+
+        # My first though is to traverse thru every one of them and give asw
+        # but i have an advantage. i can only just keep going left. but no, they if K is huge
+        # thinking...
+        # use DFS, and stop when K elements are returned
+        var = []
+
+
+        def dfs(root):
+            if root is None:
+                return
+
+            if len(var) == k: return
+
+            dfs(root.left); var.append(root.val) if len(var) < k else None; dfs(root.right)
+        
+        dfs(root)
+        print(var)
+
+
+        return var[k-1]
+        
+        
+
+
+
+
