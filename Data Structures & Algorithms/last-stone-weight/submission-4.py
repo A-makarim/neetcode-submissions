4ff01c084417d06604724ab1f5abc3c42ed55f95
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-04-25 17:37:01 +0100

    Add: last-stone-weight - submission-4

---
+++ Data Structures & Algorithms/last-stone-weight/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        # heappop will give lowest, we need highest, flip signs 
+
+        stones = [-s for s in stones]
+
+        # pop first
+        heapq.heapify(stones)
+        print(stones)
+        first = heapq.heappop(stones)
+
+        #keep destroying till last 
+
+        while len(stones) >= 1:
+            print(stones)
+            second = heapq.heappop(stones)               # -2
+            smash = first - second   # -7+3 is -4       -4 +2  is -2
+
+            if smash != 0: 
+                heapq.heappush(stones, smash)  # -4
+            if not stones: break
+            first = heapq.heappop(stones)              #-4
+        return abs(first)
+
+
+
