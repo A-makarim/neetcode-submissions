df41ebb0a8a6772c76e72ad27f53cf512c39150b
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-04-30 17:36:22 +0100

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # each number can be used at most once, which means you can not include, or pop and move on
+        # there was be duplicate numbers?
+    
+        res = []; sublist = []
+
+        def dfs(i):
+
+            if sum(sublist) == target:
+                if sublist not in res:
+                    res.append(list(sublist))
+                return
+            
+            if sum(sublist) > target:
+                return
+            
+            if i >= len(candidates):
+                return
+
+            sublist.append(candidates[i])
+            dfs(i+1)
+
+            sublist.pop()
+            dfs(i+1)
+
+        dfs(0); return res
+
+            
+        
+        
