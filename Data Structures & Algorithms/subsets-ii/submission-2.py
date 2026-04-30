2273c4d9d3865178401ec19de66a4ccba93bb6be
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-04-30 23:35:25 +0100

    Add: subsets-ii - submission-2

---
+++ Data Structures & Algorithms/subsets-ii/submission-2.py
@@ -0,0 +1,33 @@
+"""
+KEY INSIGHTS
+IN REPEATED ELEMENTS, USE WHILE TO SKIP THE POP RECURSION, 
+BECAUSE YOU'VE SORTED THE LIST ALREADY
+IF YOU POP AND PUSH THE SAME ELEMENT AGAIN
+YOU ARE ESSENTIALLY DOING THE SAME SUBSET!!!
+"""
+
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # this is diff from subset as it can contain duplicates. 
+
+        res = []
+        sublist = []
+        nums.sort()
+
+        def dfs(i):
+            if i == len(nums):
+                res.append(list(sublist))
+                return
+
+            sublist.append(nums[i])
+            dfs(i+1)
+
+            sublist.pop()
+            while i + 1 < len(nums) and nums[i] == nums[i+1]:
+                i +=1
+            dfs(i+1)  # here is the thing. if you i is a duplicate element, you 
+        
+        dfs(0)
+        return res
