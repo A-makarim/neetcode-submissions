6e4635ff915882cdf21bc97ce5e64bf5345af5b9
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-05-01 19:12:28 +0100

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        # permutation means:
+        #   you have let's say 7 players
+        #   what orders you can arrange them in 
+        #   we just did combination
+        #   but here order doesn't matter AT ALL (which is honestly great)
+
+        # take a num, move on, don't take a num, move on
+
+        if len(nums) == 0:
+            return [[]]
+
+        sublists = self.permute(nums[1:])   # re call permute but remove first element
+        res = []
+
+        for alist in sublists:
+            for i in range(len(alist)+1 ):
+                alist_copy = alist.copy()
+                alist_copy.insert(i , nums[0])
+                res.append(alist_copy)
+
+        return res
+
+        dfs(0)
+        return res
+            
