56157a5348ae30b78f5de2a8201f575fbbaebb94
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-07-01 16:14:25 +0100

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        match = {')':'(', ']':'[', '}':'{'}   
+
+        stack = []  
+
+        for char in s:
+            if char in "([{":
+                stack.append(char)           # ad opening bracket
+
+            else:                                # if not opening bracket
+                if not stack:                    # if you encounter a closing bracket before
+                    return False
+                if stack[-1] != match[char]:   # you need a closing bracket for last element in stack
+                    return False
+                stack.pop()                      # remove the last one from stack
+
+        return len(stack) == 0              #at one point you will have removed all opening brackets 
