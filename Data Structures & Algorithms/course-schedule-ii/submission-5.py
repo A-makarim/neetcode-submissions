6c6410b8d9bd4dcf1533159d2189c8d4da7ac94f
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-07-04 18:08:47 +0100

    Add: course-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-5.py
@@ -0,0 +1,43 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        premap = {}
+        for i in prerequisites:
+            if i[0] in premap:
+                premap[i[0]].append(i[1])
+            else: 
+                premap[i[0]] = [i[1]]
+        taken = []
+        totake = []
+        x = True
+        order = []
+        print(premap)
+
+        def dfs(n):
+            nonlocal totake, taken, x, order
+            if n in taken:
+                return True
+            if n not in premap.keys():
+                if n not in taken:
+                    taken.append(n)
+                    order.append(n)
+                return True
+            if n in premap.keys():
+                for i in premap[n]: #1
+                    if i in totake:
+                        return False
+                    totake.append(i)  # have to take 0
+                    x = dfs(i)   # 0 will get appended again
+                    if x == False:
+                        break
+                    totake.remove(i)
+                    taken.append(i)
+                    order.append(n)
+
+            return x
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+
+        return order
+        
