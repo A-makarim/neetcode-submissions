bc07382809fa383db897c47e3b62072e7d3c2568
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-07-13 19:52:46 +0100

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        dp = [False] * (len(s)+1)   # for a 3 letter word, 0 ,1 , 2, 3
+
+        # the last segment cna be true
+        dp[len(s)] = True    # should be able to segment as 3. it's an empty string
+
+        for i in range(len(s)-1, -1, -1):  # goes till zero, with -1 step
+            for w in wordDict: # take one word:
+                x = len(w)    # let's check if this word in last suffix
+                if (i + x <= len(s)) and s[i: i +x] == w:
+                    dp[i] = dp[i+x]
+                if dp[i]:
+                    break
+        return dp[0]
+        
