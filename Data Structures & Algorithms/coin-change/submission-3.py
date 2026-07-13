c164aceab965342b2f4270869128e34c2049a598
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-07-13 17:54:35 +0100

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        # for every state, we need the min coins
+
+        dp = [float("inf")] * (amount + 1)
+
+        dp [0] = 0
+
+        print(dp)
+
+        def dfs(amount):
+            if amount < 0: # you can not use this path, return inf, 
+                return float('inf')
+
+            if dp[amount] != float('inf'): # you can use pre computed values
+                return dp[amount]
+
+            ans = float('inf') # this can be infinity
+            for i in coins: # iterate on every coin
+                ans = min( 1 + dfs(amount - i), ans)
+            
+            dp[amount] = ans
+            return ans
+
+
+        x = dfs(amount)
+        return x if x != float('inf') else -1
+
+
+        
+
