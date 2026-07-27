18443797e16abecaadbae2b46e9f5c2c3766c310
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-07-27 18:50:34 +0100

    Add: interleaving-string - submission-2

---
+++ Data Structures & Algorithms/interleaving-string/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        lens1 = len(s1)
+        lens2 = len(s2)
+        lens3 = len(s3)
+        dp = {}
+        def dfs(i , j):
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if i + j == lens3:
+                dp[(i, j)] = True
+                return dp[(i, j)]
+
+            if i < lens1:
+                if s1[i] == s3[i+j]:
+                    dp[(i, j)] = dfs(i+1,j)
+                    if dp[(i, j)]:
+                        return dp[(i, j)]
+            if j < lens2:
+                if s2[j] == s3[i+j]:
+                    dp[(i, j)] = dfs(i, j+1)
+                    if dp[(i, j)]:
+                        return dp[(i, j)]
+            dp[(i, j)] = False
+            return False     
+                   
+        if lens1 + lens2 != lens3:
+            return False
+        return dfs(0,0)
+
+        
