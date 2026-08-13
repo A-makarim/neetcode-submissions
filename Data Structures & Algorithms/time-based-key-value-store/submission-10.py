64c335619a70a02865d1c402c5f3f1d89a20171a
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-08-13 12:45:52 +0100

    Add: time-based-key-value-store - submission-10

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-10.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+class TimeMap:
+
+    def __init__(self):
+        self.h = defaultdict(list)
+
+    # list is alr sorted
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.h[key].append([value, timestamp])
+        return
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        
+        values = self.h.get(key, [])
+        x = len(values)
+        l, r = 0 , x -1
+        while l <= r:
+            m = (l+r) // 2
+            if self.h[key][m][1] <= timestamp:
+                res = self.h[key][m][0]
+
+                l = m +1
+            else:
+                r = m-1
+            
+        return res
+
+
+        for i in self.h[key]: 
+            if i[1] == timestamp:  # iterating over values 10 not equal to 1
+                return i[0]
+
+        # first loops gives the valeu at that timestmap. now you might now find that timestamp.
+        # find largest before it 
+        maxx = 0 
+        for i in self.h[key]:
+            if i[1] < timestamp:
+                maxx = max( maxx, i[1]) # max is 10 we keep max 10
+                
+
+        for i in self.h[key]:   # 
+            
+            if i[1] == maxx:   # not max
+                return i[0]
+        return ""
+        
