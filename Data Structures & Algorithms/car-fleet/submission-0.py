165673f3e013dc3b0d3cf16003e2ec4bef0ae772
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-08-15 18:30:20 +0100

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        # we need the car with max max pos
+        pair = [[p,s] for p,s in zip(position, speed)]
+        pair.sort(key = lambda x: x[0], reverse = True)
+        stack = []
+        for i, j in pair:
+            t = (target - i) / j
+            stack.append(t)
+            if len(stack) >=2:
+                if stack[-1] > stack[-2]:
+                    stack.pop()
+        return len(stack) 
+
+        
+
+
+
+        
