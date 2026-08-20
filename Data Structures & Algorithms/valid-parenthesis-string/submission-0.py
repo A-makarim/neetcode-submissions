3c62faad88a205896cb8270276486d2af057f160
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-08-20 09:42:25 +0100

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+
+        minopening = 0
+        maxopening = 0
+
+        for i in s:
+            if i == "(":
+                minopening +=1
+                maxopening +=1
+            if i == ")":
+                minopening -=1
+                maxopening -=1
+            if i == "8":
+                minopening -=1
+                maxopening +=1
+            if maxopening < 0: # meanings wwe have more closing brackets
+                return False
+            if minopening < 0: # we have more opening brackets
+                minopening = 0 # reset to zero as we can chose not to use an opening bracket. more closing brackets alr reject at first
+            
+        return True if minopening == 0 else False
+
+
+
+x = Solution()
+assert x.checkValidString("(()*)") == True
+        
