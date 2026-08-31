09d833475e12baa2096a54848403d9ea8b8bcc12
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-08-31 17:31:39 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        lens1 = len(s1)
+        l = 0
+        r = lens1 -1
+        
+        hashmap = Counter(s1)
+        substring = s2[l:r+1]
+        hashmap2 = Counter(substring)
+
+        while r < len(s2):
+            for i in hashmap:
+                if hashmap[i] != hashmap2[i]:
+                    break
+            else:
+                return True
+
+            left = s2[l]
+            hashmap2[left] -=1
+            l+=1
+            r+=1
+            if r == len(s2):
+                break
+            print(l, r)
+            right = s2[r]
+            hashmap2[right] = hashmap2.get(right, 0)
+            hashmap2[right] +=1
+        return False
+
+        
