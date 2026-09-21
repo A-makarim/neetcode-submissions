56503cc606db671ee0e83bcaa76958a29d938b91
Abdul Azeem Makarim <114302821+A-makarim@users.noreply.github.com>
2026-09-21 21:13:45 +0100

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        n = len(intervals)
+        res = []
+        i = 1
+        first = intervals[0]
+        while i < n:
+            # we will merge two at a time
+            second = intervals[i]
+            if first[1] < second[0]:
+                res.append(second)
+                i+=1
+                first = second
+                print(first)
+                
+            else:
+                newstart = min(first[0], second[0])
+                newend = max(first[1], second[1])
+                res.append([newstart, newend])
+                i+=1
+                first = [newstart, newend]
+        return res
